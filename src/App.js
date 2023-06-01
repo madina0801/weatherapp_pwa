db982efecaf699db1597710dc1709fb32d1c5b33
Madina <madinatussupova0820@gmail.com>
2023-06-01 13:24:00 +0600

    complete react part

--- src/App.js
+++ src/App.js
@@ -5,16 +5,45 @@ import './App.css'
 
 const App = () => {
 	const [query, setQuery] = useState('');
+	const [weather, setWeather] = useState({});
+
+	const search = async (e) => {
+		if (e.key === 'Enter') {
+			const data = await fetchWeather(query);
+			console.log(data);
+			setWeather(data);
+			setQuery('');
+		}
+	}
 
 	return (
 		<div className="main-container">
 			<input
 				type="text"
 				className="search"
-				placeholder="Search ..."
+				placeholder="Search Your City"
 				value={query}
 				onChange={(e) => setQuery(e.target.value)}
+				onKeyDown={search}
 			/>
+
+			{weather.main && (
+				<div className="city">
+					<h2 className="city-name">
+						<span>{weather.name}</span>
+						<sup>{weather.sys.country}</sup>
+					</h2>
+					<div className="city-temp">
+						{Math.round(weather.main.temp)}
+						<sup>&deg;</sup>C
+					</div>
+
+					<div className="info">
+						<img className="city-icon" src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}.png`} alt={weather.weather[0].description}/>
+						<p>{weather.weather[0].description}</p>
+					</div>
+				</div>
+			)}
 		</div>
 	)
 }
--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 const URL = 'https://api.openweathermap.org/data/2.5/weather';
 const API_KEY = process.env.REACT_APP_API_KEY;
-console.log(API_KEY);
 
 export const fetchWeather = async(query) => {
 	const {data} = await axios.get(URL, {

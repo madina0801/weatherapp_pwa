b91c807f63e7e27d384a4e049830eff1b2b00be0
Madina <madinatussupova0820@gmail.com>
2023-06-01 09:34:14 +0600

    add styles

--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -3,12 +3,14 @@ import axios from 'axios';
 const URL = 'https://api.openweathermap.org/data/2.5/weather';
 const API_KEY = '4656d147fd60979d82696e05f9f45ba7'
 
-const fetchWeather = async(query) => {
-	const response = await axios.get(URL, {
+export const fetchWeather = async(query) => {
+	const {data} = await axios.get(URL, {
 		params: {
 			q: query,
 			units: 'metric',
 			APPID: API_KEY,
 		}
 	})
+
+	return data;
 }

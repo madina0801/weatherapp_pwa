152e8b8e0f0eccd309a919fbff55642fae75a507
Madina <madinatussupova0820@gmail.com>
2023-06-02 12:47:31 +0600

    install service worker and listen to requests

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,8 +1,31 @@
 const CACHE_NAME = 'version-1';
 const urlsToCache = ['index.html', 'offline.html'];
+const self = this;
 
 // Install Service Worker
+self.addEventListener('install', (event) => {
+	event.waitUntil(
+		caches.open(CACHE_NAME)
+			.then((cache) => {
+				console.log('Opened cache');
+
+				cache.addAll(urlsToCache);
+			})
+	)
+})
 
 // Listen to requests
+self.addEventListener('fetch', (event) => {
+	event.respondWith(
+		caches.match(event.request)
+			.then(() => {
+				return fetch(event.request)
+					.catch(() => caches.match('offline.html'))
+			})
+	)
+})
 
 // Activate Service Worker
+self.addEventListener('activate', (event) => {
+
+})

84afc0deaf4838e1a5c5322384816cb266f86e71
Madina <madinatussupova0820@gmail.com>
2023-06-15 13:36:27 +0600

    service worker bug

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,20 +1,21 @@
-const CACHE_NAME = 'version-1';
+const CACHE_NAME = "version-1";
 const urlsToCache = ['index.html', 'offline.html'];
+
 const self = this;
 
-// Install Service Worker
+// Install SW
 self.addEventListener('install', (event) => {
 	event.waitUntil(
 		caches.open(CACHE_NAME)
 			.then((cache) => {
 				console.log('Opened cache');
 
-				cache.addAll(urlsToCache);
+				return cache.addAll(urlsToCache);
 			})
 	)
-})
+});
 
-// Listen to requests
+// Listen for requests
 self.addEventListener('fetch', (event) => {
 	event.respondWith(
 		caches.match(event.request)
@@ -23,20 +24,21 @@ self.addEventListener('fetch', (event) => {
 					.catch(() => caches.match('offline.html'))
 			})
 	)
-})
+});
 
-// Activate Service Worker
+// Activate the SW
 self.addEventListener('activate', (event) => {
 	const cacheWhitelist = [];
 	cacheWhitelist.push(CACHE_NAME);
 
 	event.waitUntil(
 		caches.keys().then((cacheNames) => Promise.all(
-			cacheNames.map(cacheName => {
-				if(!cacheWhitelist.includes(cacheName)) {
+			cacheNames.map((cacheName) => {
+				if (!cacheWhitelist.includes(cacheName)) {
 					return caches.delete(cacheName);
 				}
 			})
 		))
+
 	)
-})
+});
